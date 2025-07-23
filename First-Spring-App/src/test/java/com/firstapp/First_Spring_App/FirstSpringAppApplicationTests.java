e71500c146f45fc90ee8c6dd3ee84b5d6f4456de
YASHMANE96 <pradiego9687@gmail.com>
2025-07-23 16:20:44 +0530

    First Spring App Demo

---
+++ First-Spring-App/src/test/java/com/firstapp/First_Spring_App/FirstSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstapp.First_Spring_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
